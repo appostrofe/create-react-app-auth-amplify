e5c7da3a44adb41d3653e57d3bdea5dce9cd1111
Antonio Sotomayor <antonio.sotomayor.martinez@gmail.com>
2020-07-10 16:19:24 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { withAuthenticator } from 'aws-amplify-react'
-import Amplify, { Auth } from 'aws-amplify';
-import aws_exports from './aws-exports';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { withAuthenticator } from "aws-amplify-react";
+import Amplify, { Auth } from "aws-amplify";
+import aws_exports from "./aws-exports";
+
 Amplify.configure(aws_exports);
+
+const federated = {
+  google_client_id:
+    "254192244285-9gba77uanfstfmcgv0q2c40ucv81657g.apps.googleusercontent.com",
+  googleClientId:
+    "254192244285-9gba77uanfstfmcgv0q2c40ucv81657g.apps.googleusercontent.com",
+  oauthConfig: {
+    redirectSignIn: "http://localhost:3000/",
+    redirectSignOut: "http://localhost:3000/",
+  },
+};
 
 class App extends Component {
   render() {
@@ -29,4 +41,12 @@ class App extends Component {
   }
 }
 
-export default withAuthenticator(App, true);
+export default withAuthenticator(App, {
+  federated: federated,
+  signUpConfig: {
+    hiddenDefaults: ["phone_number"],
+    signUpFields: [
+      { label: "Name", key: "name", required: true, type: "string" },
+    ],
+  },
+});
